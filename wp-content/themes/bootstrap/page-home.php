4b71d6a2eb0cc61b62f6663f7aa3eee8db772b55
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-12-23 15:11:31 +0200

    Lecture 61: Code the Testimonials with Custom Post Types [Deliver #110655436]

--- wp-content/themes/bootstrap/page-home.php
+++ wp-content/themes/bootstrap/page-home.php
@@ -3,56 +3,49 @@
     Template Name: Home Page
  */
 //Custom Fields                         page_id, custom_name, string
-$prelaunch_price        =  get_post_meta(7, 'prelaunch_price', true);
-$launch_price           =  get_post_meta(7, 'launch_price', true);
-$final_price            =  get_post_meta(7, 'final_price', true);
-$course_url             =  get_post_meta(7, 'course_url', true);
-$button_text            =  get_post_meta(7, 'button_text', true);
+$prelaunch_price = get_post_meta(7, 'prelaunch_price', true);
+$launch_price = get_post_meta(7, 'launch_price', true);
+$final_price = get_post_meta(7, 'final_price', true);
+$course_url = get_post_meta(7, 'course_url', true);
+$button_text = get_post_meta(7, 'button_text', true);
 
-$optin_text             =  get_post_meta(7, 'optin_text', true);
-$optin_button_text      =  get_post_meta(7, 'optin_button_text', true);
+$optin_text = get_post_meta(7, 'optin_text', true);
+$optin_button_text = get_post_meta(7, 'optin_button_text', true);
 
 //Advanced Custom Fields
-$income_feature_image   = get_field('income_feature_image');
-$income_section_title   = get_field('income_section_title');
-$income_section_desc    = get_field('income_section_description');
-$reason_1_title         = get_field('reason_1_title');
-$reason_1_description   = get_field('reason_1_description');
-$reason_2_title         = get_field('reason_2_title');
-$reason_2_description   = get_field('reason_2_description');
+$income_feature_image = get_field('income_feature_image');
+$income_section_title = get_field('income_section_title');
+$income_section_desc = get_field('income_section_description');
+$reason_1_title = get_field('reason_1_title');
+$reason_1_description = get_field('reason_1_description');
+$reason_2_title = get_field('reason_2_title');
+$reason_2_description = get_field('reason_2_description');
 
-$who_feature_image      = get_field('who_feature_image');
-$who_section_title      = get_field('who_section_title');
-$who_section_body       = get_field('who_section_body');
+$who_feature_image = get_field('who_feature_image');
+$who_section_title = get_field('who_section_title');
+$who_section_body = get_field('who_section_body');
 
 $features_section_image = get_field('features_section_image');
 $features_section_title = get_field('features_section_title');
-$features_section_body  = get_field('features_section_body');
-$course_feature_icon    = get_field('course_feature_icon');
+$features_section_body = get_field('features_section_body');
+$course_feature_icon = get_field('course_feature_icon');
 
 $project_features_title = get_field('project_feature_title');
-$project_feature_body   = get_field('project_feature_body');
+$project_feature_body = get_field('project_feature_body');
 
-$featurette_title       = get_field('featurette_title');
-$featurette_video_link  = get_field('featurette_video_link');
+$featurette_title = get_field('featurette_title');
+$featurette_video_link = get_field('featurette_video_link');
 
 $instructor_section_title = get_field('instructor_section_title');
-$instructor_name          = get_field('instructor_name');
-$bio_excerpt              = get_field('bio_excerpt');
-$full_bio                 = get_field('full_bio');
-$twitter_username         = get_field('twitter_username');
-$google_plus_username     = get_field('google_plus_username');
-$facebook_username        = get_field('facebook_username');
-$num_students             = get_field('num_students');
-$num_reviews              = get_field('num_reviews');
-$num_courses              = get_field('num_courses');
-
-
-
-
-
-
-
+$instructor_name = get_field('instructor_name');
+$bio_excerpt = get_field('bio_excerpt');
+$full_bio = get_field('full_bio');
+$twitter_username = get_field('twitter_username');
+$google_plus_username = get_field('google_plus_username');
+$facebook_username = get_field('facebook_username');
+$num_students = get_field('num_students');
+$num_reviews = get_field('num_reviews');
+$num_courses = get_field('num_courses');
 
 
 //WP Header
@@ -65,7 +58,8 @@ get_header(); ?>
             <div class="row">
 
                 <div class="col-sm-5">
-                    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo-badge.png" alt="BootStrap Wordress" class="logo">
+                    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo-badge.png"
+                         alt="BootStrap Wordress" class="logo">
                 </div>
                 <!--row-->
 
@@ -101,7 +95,8 @@ get_header(); ?>
 
                     </div>
                     <!--price-timeline-->
-                    <p><a class="btn btn-lg btn-danger" href="<?php echo $course_url; ?>" role="button"><?php echo $button_text; ?></a></p>
+                    <p><a class="btn btn-lg btn-danger" href="<?php echo $course_url; ?>"
+                          role="button"><?php echo $button_text; ?></a></p>
                 </div>
 
             </div>
@@ -118,7 +113,8 @@ get_header(); ?>
             </div>
 
             <div class="col-sm-4">
-                <button class="btn btn-success btn-lg btn-block" data-toggle="modal" data-target="#myModal"><?php echo $optin_button_text; ?></button>
+                <button class="btn btn-success btn-lg btn-block" data-toggle="modal"
+                        data-target="#myModal"><?php echo $optin_button_text; ?></button>
             </div>
         </div>
     </div>
@@ -129,8 +125,9 @@ get_header(); ?>
     <div class="container">
         <div class="section-header">
 
-            <?php if(!empty($income_feature_image)) : ?>
-                <img src="<?php echo $income_feature_image['url']; ?>" alt="<?php echo $income_feature_image['alt']; ?>">
+            <?php if (!empty($income_feature_image)) : ?>
+                <img src="<?php echo $income_feature_image['url']; ?>"
+                     alt="<?php echo $income_feature_image['alt']; ?>">
             <?php endif; ?>
 
 
@@ -164,7 +161,7 @@ get_header(); ?>
 <section id="who-benefits">
     <div class="container">
         <div class="section-header">
-            <?php if(!empty($who_feature_image)) : ?>
+            <?php if (!empty($who_feature_image)) : ?>
                 <img src="<?php echo $who_feature_image['url']; ?>" alt="<?php echo $who_feature_image['alt']; ?>">
             <?php endif; ?>
 
@@ -186,27 +183,29 @@ get_header(); ?>
 <section id="course-features">
     <div class="container">
         <div class="section-header">
-            <?php if(!empty($features_section_image)) : ?>
-                <img src="<?php echo $features_section_image['url']; ?>" alt="<?php echo $features_section_image['alt']; ?>">
+            <?php if (!empty($features_section_image)) : ?>
+                <img src="<?php echo $features_section_image['url']; ?>"
+                     alt="<?php echo $features_section_image['alt']; ?>">
             <?php endif; ?>
 
             <h2><?php echo $features_section_title; ?></h2>
 
-            <?php if(!empty($features_section_body)) : ?>
-            <p class="lead"><?php echo $features_section_body; ?></p>
+            <?php if (!empty($features_section_body)) : ?>
+                <p class="lead"><?php echo $features_section_body; ?></p>
             <?php endif; ?>
         </div>
         <!--section-header-->
 
         <div class="row">
-            <?php $loop = new WP_Query( array('post_type' => 'course_feature', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
+            <?php $loop = new WP_Query(array('post_type' => 'course_feature', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
             <?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
                 <div class="col-sm-2">
                     <i class="<?php the_field('course_feature_icon'); ?>"></i>
                     <h4><?php the_title(); ?></h4>
                 </div>
-            <?php endwhile; wp_reset_query(); ?>
+            <?php endwhile;
+            wp_reset_query(); ?>
 
         </div>
         <!--row-->
@@ -223,14 +222,14 @@ get_header(); ?>
         <p class="lead"><?php echo $project_feature_body; ?></p>
 
         <div class="row">
-            <?php $loop = new WP_Query( array('post_type' => 'project_feature', 'orderby' => 'post_id', 'order' => 'ASC') ); ?>
+            <?php $loop = new WP_Query(array('post_type' => 'project_feature', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
             <?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
                 <div class="col-sm-4">
                     <?php
-                        if (has_post_thumbnail()) {
-                            the_post_thumbnail();
-                        }
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail();
+                    }
                     ?>
 
                     <h3><?php the_title(); ?></h3>
@@ -238,8 +237,9 @@ get_header(); ?>
                     <p><?php the_content(); ?></p>
                 </div>
 
-            <?php endwhile; wp_reset_query(); ?>
-            
+            <?php endwhile;
+            wp_reset_query(); ?>
+
         </div>
         <!--row-->
 
@@ -275,24 +275,28 @@ get_header(); ?>
                     </div>
 
                     <div class="col-lg-4">
-                        <?php if(!empty($twitter_username)) : ?>
-                        <a href="https://twitter.com/<?php echo $twitter_username; ?>" target="_blank" class="badge social twitter"><i
-                                class="fa fa-twitter"></i></a>
+                        <?php if (!empty($twitter_username)) : ?>
+                            <a href="https://twitter.com/<?php echo $twitter_username; ?>" target="_blank"
+                               class="badge social twitter"><i
+                                    class="fa fa-twitter"></i></a>
                         <?php endif; ?>
 
-                        <?php if(!empty($facebook_username)) : ?>
-                        <a href="https://facebook.com/<?php echo $facebook_username; ?>" target="_blank" class="badge social facebook"><i
-                                class="fa fa-facebook"></i></a>
+                        <?php if (!empty($facebook_username)) : ?>
+                            <a href="https://facebook.com/<?php echo $facebook_username; ?>" target="_blank"
+                               class="badge social facebook"><i
+                                    class="fa fa-facebook"></i></a>
                         <?php endif; ?>
 
-                        <?php if(!empty($google_plus_username)) : ?>
-                        <a href="https://plus.google.com/<?php echo $google_plus_username; ?>" target="_blank" class="badge social gplus"><i
-                                class="fa fa-google-plus"></i></a>
-                        <?php endif;  ?>
+                        <?php if (!empty($google_plus_username)) : ?>
+                            <a href="https://plus.google.com/<?php echo $google_plus_username; ?>" target="_blank"
+                               class="badge social gplus"><i
+                                    class="fa fa-google-plus"></i></a>
+                        <?php endif; ?>
                     </div>
                 </div>
                 <!--row-->
                 <p class="lead"><?php echo $bio_excerpt; ?></p>
+
                 <p><?php echo $full_bio; ?></p>
 
                 <h3>The Numbers
@@ -321,7 +325,8 @@ get_header(); ?>
                             </div>
                         </div>
                     </div>
-                </div> <!--row-->
+                </div>
+                <!--row-->
             </div>
         </div>
         <!--row-->
@@ -338,65 +343,34 @@ get_header(); ?>
 
                 <h2>What People Are Saying About Brad</h2>
 
-                <!--TESTIMONIAL-->
-                <div class="row testimonial">
-                    <div class="col-sm-4">
-                        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/brennan.jpg" alt="Brennan">
-                    </div>
-                    <div class="col-sm-8">
-                        <blockquote>
-                            Greate video course!
-                            <cite>
-                                &mdash; Brennan, graduate of all of Brad's courses
-                            </cite>
-                        </blockquote>
-                    </div>
-                </div> <!--row-->
+                <?php $loop = new WP_Query(array('post_type' => 'testimonial', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
+                <?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
-                <div class="row testimonial">
-                    <div class="col-sm-4">
-                        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ben.png" alt="Illustratrion of a man with a moustache">
-                    </div>
-                    <div class="col-sm-8">
-                        <blockquote>
-                            Greate video course!
-                            <cite>
-                                &mdash; Ben, graduate of Build a Website from Scratch with HTML &amp; CSS
-                            </cite>
-                        </blockquote>
-                    </div>
-                </div> <!--row-->
+                    <!--TESTIMONIAL-->
+                    <div class="row testimonial">
+                        <div class="col-sm-4">
+                            <?php
+                            if (has_post_thumbnail()) {
+                                the_post_thumbnail(array(200, 200));
+                            }
+                            ?>
+                        </div>
+                        <div class="col-sm-8">
+                            <blockquote>
+                                <?php the_content(); ?>
+                                <cite>
+                                    &mdash; <?php the_title(); ?>
+                                </cite>
+                            </blockquote>
+                        </div>
+                    </div> <!--row-->
 
-                <div class="row testimonial">
-                    <div class="col-sm-4">
-                        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/aj.png" alt="Illustratrion of a man with a beard">
-                    </div>
-                    <div class="col-sm-8">
-                        <blockquote>
-                            Greate video course!
-                            <cite>
-                                &mdash; AJ, graduate of Code a Responsive Website with Bootstrap 3
-                            </cite>
-                        </blockquote>
-                    </div>
-                </div> <!--row-->
-
-                <div class="row testimonial">
-                    <div class="col-sm-4">
-                        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/ernest.png" alt="Illustratrion of a man with a beard">
-                    </div>
-                    <div class="col-sm-8">
-                        <blockquote>
-                            Greate video course!
-                            <cite>
-                                &mdash; Ernest, graduate of Code Dynamic Websites with PHP
-                            </cite>
-                        </blockquote>
-                    </div>
-                </div> <!--row-->
+                <?php endwhile; ?>
             </div>
-        </div> <!--row-->
-    </div> <!--container-->
+        </div>
+        <!--row-->
+    </div>
+    <!--container-->
 </section>
 
 <?php
