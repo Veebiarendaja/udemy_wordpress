c274c086a6bce265b721854723ab5483db205ed7
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-12-23 16:05:18 +0200

    Lecture 62: Clean Your Template Using get_template_part() [Deliver #110655444]

---
+++ wp-content/themes/bootstrap/content-hero.php
@@ -0,0 +1,61 @@
+<?php
+//Custom Fields                         page_id, custom_name, string
+$prelaunch_price = get_post_meta(7, 'prelaunch_price', true);
+$launch_price = get_post_meta(7, 'launch_price', true);
+$final_price = get_post_meta(7, 'final_price', true);
+$course_url = get_post_meta(7, 'course_url', true);
+$button_text = get_post_meta(7, 'button_text', true);
+?>
+
+<!--HERO ===================================-->
+<section id="hero" data-type="background" data-speed="5">
+    <article>
+        <div class="container clearfix">
+            <div class="row">
+
+                <div class="col-sm-5">
+                    <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo-badge.png"
+                         alt="BootStrap Wordress" class="logo">
+                </div>
+                <!--row-->
+
+                <div class="col-sm-7 hero-text">
+                    <h1><?php bloginfo('name'); ?></h1>
+
+                    <p class="lead"><?php bloginfo('description'); ?></p>
+
+                    <div id="price-timeline">
+                        <div class="price active">
+                            <h4>Pre-launch Price
+                                <small>Buy!</small>
+                            </h4>
+                            <span><?php echo $prelaunch_price; ?></span>
+                        </div>
+                        <!--price-->
+
+                        <div class="price">
+                            <h4>Launch Price
+                                <small>Buy!</small>
+                            </h4>
+                            <span><?php echo $launch_price; ?></span>
+                        </div>
+                        <!--price-->
+
+                        <div class="price">
+                            <h4>Final Price
+                                <small>Buy!</small>
+                            </h4>
+                            <span><?php echo $final_price; ?></span>
+                        </div>
+                        <!--price-->
+
+                    </div>
+                    <!--price-timeline-->
+                    <p><a class="btn btn-lg btn-danger" href="<?php echo $course_url; ?>"
+                          role="button"><?php echo $button_text; ?></a></p>
+                </div>
+
+            </div>
+        </div>
+    </article>
+</section>
