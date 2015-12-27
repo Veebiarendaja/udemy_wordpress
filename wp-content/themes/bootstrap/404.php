57e8ad1edac8e41a6b0855c9fbe13165243056fc
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-12-27 16:35:40 +0200

    Lecture 76: Create a Useful & Engaging 404 Page [Deliver #110723040]

--- wp-content/themes/bootstrap/404.php
+++ wp-content/themes/bootstrap/404.php
@@ -8,56 +8,86 @@
  */
 
 get_header(); ?>
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+		<h1 class="page-title">That Page Can't be found!</h1>
+	</section>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div class="container">
+		<div id="primary" class="row">
+			<main id="content" class="col-sm-8">
+				<div class="error-404 not-found">
+					<div class="page-content">
+						<h2>Don't fret! Let's get you back on track.</h2>
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bootstrap' ); ?></h1>
-				</header><!-- .page-header -->
+						<!-- RESOURCES ========================= -->
+						<h3>Resources</h3>
+						<p>Perhaps you were looking for a specific resource?</p>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bootstrap' ); ?></p>
+						<?php $loop = new WP_Query(array('post_type' => 'resource', 'orderby' => 'post_id', 'order' => 'ASC')); ?>
+						<div class="resource-row clearfix">
+							<?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
-					<?php
-						get_search_form();
+								<?php
+								$resource_image = get_field('resource_image');
+								$resource_url = get_field('resource_url');
+								$add_button = get_field('add_button');
+								$button_text = get_field('button_text');
+								?>
 
-						the_widget( 'WP_Widget_Recent_Posts' );
+								<div class="resource">
+									<img src="<?php echo $resource_image[url]; ?>" alt="<?php echo $resource_image[alt]; ?>">
 
-						if ( bootstrap_categorized_blog() ) : // Only show the widget if site has multiple categories.
-					?>
+									<h3><a href="<?php echo $resource_url; ?>"><?php the_title(); ?></a></h3>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bootstrap' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
+									<?php the_excerpt(); ?>
 
-					<?php
-						endif;
+									<?php if (!empty($button_text)) : ?>
+										<a href="<?php echo $resource_url; ?>"
+										   class="btn btn-success"><?php echo $button_text; ?></a>
+									<?php endif; ?>
+								</div>
+								<!-- resource -->
+							<?php endwhile; ?>
 
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bootstrap' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						</div>
+						<!-- resource-row -->
+						
+						<!-- CATEGORIES ========================= -->
+						<h3>Categories</h3>
+						<p>...or maybe a popular category?</p>
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+						<div class="widget widget-categories">
+							<h4 class="widget-title">Most Used Categories</h4>
+							<ul>
+								<?php wp_list_categories(array(
+									'orderby'	=>	'count',
+									'order'		=>	'DESC',
+									'show_count'=>	1,
+									'title_li'	=>	'',
+									'number'	=>	10
+								)); ?>
+							</ul>
+						</div>
+						
+						<!-- ARCHIVES ========================= -->
+						<h3>ARCHIVES</h3>
+						<p>You can always sort through our archives ...</p>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+						<?php the_widget('WP_Widget_Archives', 'title=Site Archives', 'before_title=<h4 class="widgettitle">&after_title=</h4>'); ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+						<p>Head back to <a href="<?php echo esc_url(home_url('/')); ?>">home page</a>.</p>
+
+					</div>
+					<!-- page-content -->
+				</div>
+			</main><!-- content -->
+
+			<!-- SIDEBAR ========================= -->
+			<aside class="col-sm-4">
+				<?php get_sidebar(); ?>
+			</aside>
+		</div><!-- row -->
+	</div> <!-- container -->
 
 <?php
 get_footer();
